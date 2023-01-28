bcc57e865105700f5d828f6d54b918daee6b8f7b
Phùng Đông Hưng (Hank) <donghung.viethanit@gmail.com>
2023-01-28 19:48:58 +0700

    restore client

--- stream/app.js
+++ stream/app.js
@@ -1,38 +1,35 @@
 
 function openStream(cb) {
     navigator.mediaDevices.getUserMedia({ audio: false, video: true })
-      .then(stream => {
-        cb(stream);
-      })
-      .catch(err => console.log(err));
-  }
-  
-  function playVideo(stream, idVideo) {
+        .then(stream => {
+            cb(stream);
+        })
+        .catch(err => console.log(err));
+}
+
+function playVideo(stream, idVideo) {
     const video = document.getElementById(idVideo);
     video.srcObject = stream;
     video.onloadedmetadata = function () {
-      video.play();
+        video.play();
     };
-  }
-  
-  openStream(function (stream) {
+}
+
+openStream(function (stream) {
     playVideo(stream, 'localStream')
-    var opts =  { initiator: HOST, trickle: false, stream};
-    const p = new SimplePeer(opts);
+    const p = new SimplePeer({ initiator: HOST, trickle: false, stream });
 
     p.on('signal', token => {
-      $('#txtMySignal').val(JSON.stringify(token))
+        $('#txtMySignal').val(JSON.stringify(token))
     });
-  
+
     $('#btnConnect').click(() => {
-      const friendSignal = JSON.parse($('#txtFriendSignal').val());
-    //   console.log(friendSignal)
-      p.signal(friendSignal);
+        const friendSignal = JSON.parse($('#txtFriendSignal').val());
+        console.log(friendSignal)
+        p.signal(friendSignal);
     });
-    if(HOST){
-        p.on('stream', friendStream => playVideo(friendStream, 'friendStream'));
-    }
-  });
-  
-  
-  
+
+    p.on('stream', friendStream => playVideo(friendStream, 'friendStream'));
+});
+
+
