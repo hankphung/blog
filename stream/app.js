2c2597a340592e7b4f6cdefc232bffca2c40fba7
Phùng Đông Hưng (Hank) <donghung.viethanit@gmail.com>
2023-01-28 18:06:54 +0700

    adding stream webcam sharing

---
+++ stream/app.js
@@ -0,0 +1,36 @@
+
+function openStream(cb) {
+    navigator.mediaDevices.getUserMedia({ audio: false, video: true })
+      .then(stream => {
+        cb(stream);
+      })
+      .catch(err => console.log(err));
+  }
+  
+  function playVideo(stream, idVideo) {
+    const video = document.getElementById(idVideo);
+    video.srcObject = stream;
+    video.onloadedmetadata = function () {
+      video.play();
+    };
+  }
+  
+  openStream(function (stream) {
+    playVideo(stream, 'localStream')
+    const p = new SimplePeer({ initiator: !HOST, trickle: false, stream });
+  
+    p.on('signal', token => {
+      $('#txtMySignal').val(JSON.stringify(token))
+    });
+  
+    $('#btnConnect').click(() => {
+      const friendSignal = JSON.parse($('#txtFriendSignal').val());
+      console.log(friendSignal)
+      p.signal(friendSignal);
+    });
+  
+    p.on('stream', friendStream => playVideo(friendStream, 'friendStream'));
+  });
+  
+  
+  
