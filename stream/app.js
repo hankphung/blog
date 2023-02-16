164f703a0f297bf453da65568c9cb4be53c82f8e
Hank Phung <hank.phung@vinted.com>
2023-02-16 12:25:00 +0700

    allow user to hit the big bowl

--- stream/app.js
+++ stream/app.js
@@ -1,35 +1,66 @@
 
 function openStream(cb) {
-    navigator.mediaDevices.getUserMedia({ audio: false, video: { facingMode: "environment" } })
-        .then(stream => {
-            cb(stream);
-        })
-        .catch(err => console.log(err));
+  navigator.mediaDevices.getUserMedia({ audio: false, video: { facingMode: "environment" } })
+    .then(stream => {
+      cb(stream);
+    })
+    .catch(err => console.log(err));
 }
 
 function playVideo(stream, idVideo) {
-    const video = document.getElementById(idVideo);
-    video.srcObject = stream;
-    video.onloadedmetadata = function () {
-        video.play();
-    };
+  const video = document.getElementById(idVideo);
+  video.srcObject = stream;
+  video.onloadedmetadata = function () {
+    video.play();
+  };
 }
 
 openStream(function (stream) {
-    playVideo(stream, 'localStream')
-    const p = new SimplePeer({ initiator: HOST, trickle: false, stream });
+  playVideo(stream, 'localStream')
+  const p = new SimplePeer({ initiator: HOST, trickle: false, stream });
 
-    p.on('signal', token => {
-        $('#txtMySignal').val(JSON.stringify(token))
-    });
+  p.on('signal', token => {
+    $('#txtMySignal').val(JSON.stringify(token))
+  });
 
-    $('#btnConnect').click(() => {
-        const friendSignal = JSON.parse($('#txtFriendSignal').val());
-        console.log(friendSignal)
-        p.signal(friendSignal);
-    });
+  $('#btnConnect').click(() => {
+    const friendSignal = JSON.parse($('#txtFriendSignal').val());
+    console.log(friendSignal)
+    p.signal(friendSignal);
+  });
 
-    p.on('stream', friendStream => playVideo(friendStream, 'friendStream'));
+  p.on('stream', friendStream => playVideo(friendStream, 'friendStream'));
 });
 
 
+function videoFullScreen() {
+  var elem = document.getElementById("localStream");
+  openFullscreen(elem);
+}
+
+function pageFullScreen() {
+  var elem = document.documentElement;
+  openFullscreen(elem);
+}
+
+/* View in fullscreen */
+function openFullscreen(elem) {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen();
+  }
+}
+
+/* Close fullscreen */
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) { /* Safari */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) { /* IE11 */
+    document.msExitFullscreen();
+  }
+}
